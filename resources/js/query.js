bd2a60ad9700cd8304218ff3f03c3ca1f1df0a11
Abdullah Basit <abdullah.basit@hotmail.com>
2021-02-15 15:02:39 +0500

    Changed route(s) in query

--- resources/js/query.js
+++ resources/js/query.js
@@ -1,20 +1,20 @@
 export const getLiveCalls = () => {
-    return window.axios.get('/foresight/public/live/show-channels')
+    return window.axios.get('/live/show-channels')
 }
 
 export const getUserDetails = ({ queryKey }) => {
     const [_key, { authId }] = queryKey
-    return window.axios.get('/foresight/public/live/get-user/' + authId)
+    return window.axios.get('/live/get-user/' + authId)
 }
 
 export const getServer = () => {
-    return window.axios.get('/foresight/public/live/get-server')
+    return window.axios.get('/live/get-server')
 }
 
 export const listenThisCall = async ({ queryKey }) => {
     const [_key, { channel, agent, mode }] = queryKey
     console.log(channel, agent, mode)
-    const { data } = await window.axios.post(`/foresight/public/live/listen`, {
+    const { data } = await window.axios.post(`/live/listen`, {
         channel,
         agent,
         mode
